c0a419add2527b039d340a3c8929003ba06cb174
Daniel Scheibel <daniel@visualplayground.de>
2011-12-06 14:53:21 -0500

    initial commit

---
+++ include/TextureSequence.cpp
@@ -0,0 +1,103 @@
+#include "TextureSequence.h"
+
+TextureSequence::TextureSequence() : playheadPosition( 0 ), playheadFrameInc( 1 ), paused( false ), playing( true ), looping( true ){}
+
+TextureSequence::~TextureSequence(){
+    textures.clear();
+}
+
+/**
+ *  -- Begins playback of sequence
+ */
+void TextureSequence::play() {
+    paused = false;
+    playing = true;
+}
+
+/**
+ *  -- Pauses playback
+ */
+void TextureSequence::pause(){
+    paused = true;
+    playing = false;
+}
+
+/**
+ *  -- Stops playback and resets the playhead to zero
+ */
+void TextureSequence::stop(){
+    playheadPosition = 0;
+    playing = false;
+    paused = false;
+}
+
+/**
+ *  -- Call on each frame to update the playhead
+ */
+void TextureSequence::update(){
+    if( !paused && playing ){
+        int newPosition = playheadPosition + playheadFrameInc;
+        if( newPosition > totalFrames - 1 ){
+            if( looping ){
+                complete = false;
+                playheadPosition = newPosition - totalFrames;
+            } else {
+                complete = true;
+            }
+            
+        } else if( newPosition < 0 ) {
+            if( looping ){
+                complete = false;
+                playheadPosition = totalFrames - abs( newPosition );
+            } else {
+                complete = true;
+            }
+        } else {
+            complete = false;
+            playheadPosition = newPosition;
+        }
+    }
+}
+
+/**
+ *  -- Seek to a new position in the sequence
+ */
+void TextureSequence::setPlayheadPosition( int newPosition ){
+    playheadPosition = max( 0, min( newPosition, totalFrames ) );
+}
+
+void TextureSequence::createFromTextureList( vector<Texture *> textureList ){
+    textures.clear();
+    textures = textureList;
+    totalFrames = textures.size();
+}
+
+/**
+ *  -- Loads all files contained in the supplied director and creates Textures from them
+ */
+void TextureSequence::createFromDir( string filePath ){
+    textures.clear();
+    fs::path p( filePath );
+    for ( fs::directory_iterator it( p ); it != fs::directory_iterator(); ++it ){
+        if ( fs::is_regular_file( *it ) ){
+            // -- Perhaps there is a better way to ignore hidden files
+            string fileName =  it->path().filename().string();
+            if( !( fileName.compare( ".DS_Store" ) == 0 ) ){
+                textures.push_back( new Texture( loadImage( filePath + fileName ) ) );
+            }
+        }
+    }
+    totalFrames = textures.size();
+}
+
+/**
+ *  -- Loads all of the images in the supplied list of file paths
+ */
+void TextureSequence::createFromPathList( vector<string> paths ){
+    textures.clear();
+    for ( int i = 0; i < paths.size(); ++i ){
+        textures.push_back( new Texture( loadImage( paths[i] ) ) );
+    }
+    totalFrames = textures.size();
+}
+
---
+++ include/TextureSequence.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "cinder/app/App.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Utilities.h"
+#include "cinder/Filesystem.h"
+
+using namespace ci;
+using namespace ci::gl;
+using namespace ci::app;
+using namespace std;
+
+class TextureSequence
+{
+public:
+    TextureSequence();
+    ~TextureSequence();
+    
+    void stop();
+    void play();
+    void pause();
+    void update();
+    void createFromDir( string path );
+    void createFromPathList( vector<string> paths );
+    void createFromTextureList( vector<Texture *> textureList );
+    
+    int const getPlayheadFrameInc() { return playheadFrameInc; }
+    void setPlayheadFrameInc( int frames ) { playheadFrameInc = frames; }
+    
+    int const getPlayheadPosition() { return playheadPosition; }
+    void setPlayheadPosition( int newPosition );
+    
+    void setLooping( bool doLoop ) { looping = doLoop; }
+    
+    Texture* const getCurrentTexture() { return textures[ playheadPosition ]; }
+    
+    bool isPlaying() { return playing; }
+    bool isPaused() { return paused; }
+    
+protected:
+    int playheadPosition;
+    int playheadFrameInc;
+    vector<Texture *> textures;
+    
+    int totalFrames;
+    bool looping;
+    bool paused;
+    bool playing;
+    bool complete;
+};

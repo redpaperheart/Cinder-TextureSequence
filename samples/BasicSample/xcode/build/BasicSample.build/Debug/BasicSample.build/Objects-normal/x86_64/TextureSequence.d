29a903b9a0806cc5fba528d40399544757161806
adria <araidz@gmail.com>
2015-01-20 13:55:47 -0500

    removed ignored files

--- samples/BasicSample/xcode/build/BasicSample.build/Debug/BasicSample.build/Objects-normal/x86_64/TextureSequence.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/adria/DEV/CINDER/cinder_glNext/blocks/Cinder-TextureSequence/samples/BasicSample/blocks/TextureSequence/src/rph/TextureSequence.cpp \
-  /Users/adria/DEV/CINDER/cinder_glNext/blocks/Cinder-TextureSequence/samples/BasicSample/blocks/TextureSequence/src/rph/TextureSequence.h \
-  ../../../../../include/cinder/gl/Texture.h

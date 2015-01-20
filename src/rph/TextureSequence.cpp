c8521076f3768e56a570d988a188b157917aa47c
adria <araidz@gmail.com>
2015-01-20 15:23:04 -0500

    fixed duplicate files

--- src/rph/TextureSequence.cpp
+++ src/rph/TextureSequence.cpp
@@ -1,159 +1,154 @@
+/*
+ Copyright (c) 2015 Red Paper Heart Inc.
+ 
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 #include "TextureSequence.h"
 
-TextureSequence::TextureSequence() : playheadPosition( 0 ), playheadFrameInc( 1 ), paused( false ), playing( true ), looping( true ), mFps( 0.0f ), playReverse( false ){
-    
-}
+namespace rph {
 
-TextureSequence::~TextureSequence(){
-    textures.clear();
-}
+    TextureSequence::TextureSequence() {}
 
-void TextureSequence::setFlipped( bool horizontal, bool vertical ){
-    flipHorizontally = horizontal;
-    flipVertically = vertical;
-    // create a matrix here to apply before drawing;
-}
-
-/**
- *  -- Begins playback of sequence
- */
-void TextureSequence::play(bool reverse) {
-    playReverse = reverse;
-    paused = false;
-    playing = true;
-    complete = false;
-}
-
-/**
- *  -- Pauses playback
- */
-void TextureSequence::pause(){
-    paused = true;
-    playing = false;
-}
-
-/**
- *  -- Stops playback and resets the playhead to zero
- */
-void TextureSequence::stop(){
-    playheadPosition = 0;
-    playing = false;
-    paused = false;
-}
-
-/**
- *  -- Seek to a new position in the sequence
- */
-void TextureSequence::setPlayheadPosition( int newPosition ){
-    playheadPosition = max( 0, min( newPosition, totalFrames - 1 ) );
-}
-/**
- *  -- Seek to a new position in the sequence
- */
-void TextureSequence::setPlayheadPositionByPerc( float perc ){
-    perc = max( 0.0f, min( perc, 1.0f ) );
-    setPlayheadPosition( perc * (totalFrames - 1) );
-}
-
-/**
- *  -- Call on each frame to update the playhead
- */
-void TextureSequence::update(){
-        
-    if(mFps && (getElapsedSeconds()-mStartTime) < 1.0f/mFps ){
-        return;
+    TextureSequence::~TextureSequence()
+    {
+        mTextureRefs.clear();
     }
-    if( !paused && playing ){
-        int newPosition = playheadPosition + (playReverse ? -playheadFrameInc : playheadFrameInc);
-        if( newPosition > totalFrames - 1 ){
-            if( looping ){
-                complete = false;
-                playheadPosition = newPosition - totalFrames;
+    
+    void TextureSequence::setup( const std::vector<ci::gl::TextureRef> &textureRefs, const float &fps )
+    {
+        mTextureRefs.clear();
+        mTextureRefs = textureRefs;
+        mNumFrames = mTextureRefs.size();
+        setFramerate( fps );
+    }
+    
+    void TextureSequence::setFramerate( float fps )
+    {
+        mFps = fps;
+        mTimePerFrame = 1.0/mFps;
+    }
+    
+    /**
+     *  -- Begins playback of sequence
+     */
+    void TextureSequence::play( bool reverse )
+    {
+        if(mTextureRefs.size() > 0){
+            ci::app::console() << "play"<< std::endl;
+            mPlayReverse = reverse;
+            mPlaying = true;
+            mComplete = false;
+            mStartTime = ci::app::getElapsedSeconds();
+            mStartFrame = mPlayheadPosition;
+        }
+    }
+
+    /**
+     *  -- Stops/Pauses playback
+     */
+    void TextureSequence::stop()
+    {
+        mPlaying = false;
+        ci::app::console() << "stop"<< std::endl;
+    }
+
+    /**
+     *  -- Stops playback and resets the playhead to zero
+     */
+    void TextureSequence::reset()
+    {
+        mPlayheadPosition = 0;
+        mPlaying = false;
+    }
+
+    /**
+     *  -- Seek to a new position in the sequence
+     */
+    void TextureSequence::setPlayheadPosition( int newPosition )
+    {
+        mPlayheadPosition = ci::math<int>::max( 0, ci::math<int>::min( newPosition, mNumFrames - 1 ) );
+    }
+    
+    /**
+     *  -- Seek to a new position in the sequence
+     */
+    void TextureSequence::setPlayheadPositionByPerc( float perc )
+    {
+        perc = ci::math<float>::max( 0.0f, ci::math<float>::min( perc, 1.0f ) );
+        setPlayheadPosition( perc * (mNumFrames - 1) );
+    }
+
+    /**
+     *  -- TODO
+     */
+    void TextureSequence::stepForward( int frameInc ) {}
+    void TextureSequence::stepBackward(int frameInc ) {}
+    
+    /**
+     *  -- Call on each frame to update the playhead
+     */
+    void TextureSequence::update()
+    {
+        if( mPlaying ){
+            int frameToBe = mStartFrame + (mPlayReverse?-1:1) * ((ci::app::getElapsedSeconds() - mStartTime)/mTimePerFrame);
+            int frameInc = frameToBe - mPlayheadPosition;
+            //int newPosition = mPlayheadPosition + frameInc;
+            int newPosition = frameToBe;
+            
+            ci::app::console() << "1 . Frame to be: " << frameToBe << ", newPosition: "<< newPosition<< ", playheadPos: "<< mPlayheadPosition << ", frameInc: " << frameInc << std::endl;
+            
+            if( newPosition > mNumFrames - 1 ){
+                if( mLooping ){
+                    mComplete = false;
+                    mPlayheadPosition = newPosition % mNumFrames;
+                } else {
+                    mComplete = true;
+                    stop();
+                }
+                
+            } else if( newPosition < 0 ) {
+                if( mLooping ){
+                    mComplete = false;
+                    mPlayheadPosition = (mNumFrames + (newPosition % mNumFrames))%mNumFrames ;
+                    
+                } else {
+                    mComplete = true;
+                    stop();
+                }
             } else {
-                complete = true;
+                mComplete = false;
+                mPlayheadPosition = newPosition;
             }
             
-        } else if( newPosition < 0 ) {
-            if( looping ){
-                complete = false;
-                playheadPosition = totalFrames - abs( newPosition );
-            } else {
-                complete = true;
-            }
+            ci::app::console() << "2 . Frame to be: " << frameToBe << ", newPosition: "<< newPosition<< ", playheadPos: "<< mPlayheadPosition << std::endl;
+        }
+    }
+
+    ci::gl::TextureRef const TextureSequence::getCurrentTexture()
+    {
+        if( mTextureRefs.size() > 0 ){
+            return mTextureRefs[ mPlayheadPosition ];
         } else {
-            complete = false;
-            playheadPosition = newPosition;
+            return ci::gl::Texture::create(0,0);    // maybe we should return a null pointer?
         }
     }
-    mStartTime = getElapsedSeconds();
 }
-
-//void TextureSequence::draw(){
-//    gl::pushMatrices();    
-//    gl::draw(*getCurrentTexture());
-//    gl::popMatrices();
-//}
-
-
-
-void TextureSequence::createFromTextureList(const vector<Texture> &textureList, const float &fps ){
-    mStartTime = getElapsedSeconds();
-    mFps = fps;
-    textures.clear();
-    textures = textureList;
-    totalFrames = textures.size();
-}
-
-/**
- *  -- Loads all files contained in the supplied director and creates Textures from them
- */
-void TextureSequence::createFromDir(const string &filePath, const float &fps, gl::Texture::Format format ){
-    
-    mFps = fps;
-    textures.clear();
-    fs::path p( filePath );
-    for ( fs::directory_iterator it( p ); it != fs::directory_iterator(); ++it ){
-        if ( fs::is_regular_file( *it ) ){
-            // -- Perhaps there is a better way to ignore hidden files
-            string fileName = it->path().filename().string();
-            if( !( fileName.compare( ".DS_Store" ) == 0 ) ){
-                try{
-                    //try loading from resource folder
-                    textures.push_back( Texture( loadImage( loadResource( filePath + fileName ) ), format ) );
-                }catch(...){
-                    try { 
-                        // try to load relative to app
-                        textures.push_back( Texture( loadImage( loadFile( filePath + fileName ) ), format ) );
-                    }
-                    catch(...) { 
-                        try {
-                            // try to load from URL
-                            textures.push_back( Texture( loadImage( loadUrl( filePath + fileName ) ), format ) );
-                        }
-                        catch(...) {
-                            console() << getElapsedSeconds() << ":" << "TextureSequence failed to load:" << (filePath + fileName) << endl;
-                        }
-                    }
-                }
-                //textures.push_back( new Texture( loadImage( filePath + fileName ), format ) );
-            }
-        }
-    }
-    
-    mStartTime = getElapsedSeconds();
-    totalFrames = textures.size();
-}
-
-/**
- *  -- Loads all of the images in the supplied list of file paths
- */
-void TextureSequence::createFromPathList(const vector<string> &paths, const float &fps ){
-    mStartTime = getElapsedSeconds();
-    mFps = fps;
-    textures.clear();
-    for ( int i = 0; i < paths.size(); ++i ){
-        textures.push_back( Texture( loadImage( paths[i] ) ) );
-    }
-    totalFrames = textures.size();
-}
-
--- src/rph/TextureSequence.h
+++ src/rph/TextureSequence.h
@@ -1,73 +1,79 @@
-#pragma once
+/*
+ Copyright (c) 2015 Red Paper Heart Inc.
+ 
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
-#include <string>
-#include <vector>
+#pragma once
 
 #include "cinder/app/App.h"
 #include "cinder/gl/Texture.h"
-#include "cinder/ImageIo.h"
-#include "cinder/Utilities.h"
-#include "cinder/Filesystem.h"
 
-using namespace ci;
-using namespace ci::gl;
-using namespace ci::app;
-using namespace std;
+namespace rph{
 
-class TextureSequence{
-public:
-    TextureSequence();
-    ~TextureSequence();
-    
-    virtual void stop();
-    virtual void play(bool reverse = false);
-    virtual void pause();
-    virtual void update();
-    //void draw();
-    
-    // SETUP
-    virtual void createFromDir(const string &path, const float &fps = 0.0f, Texture::Format format = Texture::Format() );
-    virtual void createFromPathList(const vector<string> &paths, const float &fps = 0.0f);
-    virtual void createFromTextureList(const vector<Texture> &textureList, const float &fps = 0.0f);
-    
-    int getTotalFrames()const{ return totalFrames; }
-    
-    int getPlayheadFrameInc() const { return playheadFrameInc; }
-    void setPlayheadFrameInc( int frames ) { playheadFrameInc = frames; }
-    
-    int getPlayheadPosition() const { return playheadPosition; }
-    void setPlayheadPosition( int newPosition );
-    void setPlayheadPositionByPerc( float perc );
-    
-    void setLooping( bool doLoop ) { looping = doLoop; }
-    void setPlayReverse( bool doReverse ) { playReverse = doReverse; }
-    void setFlipped( bool horizontal, bool vertical );
-    
-    Texture const getCurrentTexture() { return textures[ playheadPosition ]; }
-    
-    bool isPlaying() { return playing; }
-    bool isPaused() { return paused; }
-    bool isEmpty(){ return textures.empty(); };
-    
-    // isDone returns true if sequence played thru and looping = false;
-    bool isComplete()const{return complete;};
-    
-protected:
-    
-    int playheadPosition;
-    int playheadFrameInc;
-    vector<Texture> textures;
-    
-    int totalFrames;
-    bool looping;
-    bool paused;
-    bool playing;
-    bool complete;
-    bool playReverse;
-    bool flipHorizontally;
-    bool flipVertically;
-    
-    float mStartTime;
-    float mFps;
-    
-};
+    class TextureSequence{
+    public:
+        TextureSequence();
+        ~TextureSequence();
+        
+        virtual void setup(const std::vector<ci::gl::TextureRef> &textureRefs, const float &fps = 0.0f);
+        virtual void update();
+
+        virtual void play(bool reverse = false);
+        virtual void stop();
+        virtual void reset();
+
+        void stepForward( int frameInc = 1 );
+        void stepBackward( int frameInc = 1 );
+        
+        void setFramerate( float fps );
+        void setPlayheadPosition( int newPosition );
+        void setPlayheadPositionByPerc( float perc );
+        void setLoop( bool doLoop )                 { mLooping = doLoop; }
+        
+        bool isPlaying()                            { return mPlaying; }                    // returns true if sequence is currently playing
+        bool isEmpty()                              { return mTextureRefs.empty(); };       // returns true if sequence is set up
+        bool isDone()const                          { return mComplete; }                   // returns true if sequence played thru and looping = false;
+        bool isLooping( )                           { return mLooping; }
+        
+        int getNumFrames()const                     { return mNumFrames; }
+        int getPlayheadPosition() const             { return mPlayheadPosition; }
+        float getFramerate()                        { return mFps; }
+        
+        ci::gl::TextureRef const getCurrentTexture();
+        
+    protected:
+        std::vector<ci::gl::TextureRef> mTextureRefs;
+        
+        int mPlayheadPosition = 0;
+        int mNumFrames = 0;
+        
+        float mFps = 0.0f;
+        float mTimePerFrame = 0.0f;
+        float mStartTime = 0.0f;
+        float mStartFrame = 0.0f;
+        
+        bool mLooping = false;
+        bool mPlaying = false;
+        bool mComplete = false;
+        bool mPlayReverse = false;
+    };
+}
